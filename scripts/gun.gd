5ec45c50ba532f0c6ab3fb43b1a0843655806f62
Fırat Sakar <firatsakar5@gmail.com>
2023-08-15 13:50:35 +0300

    gun knockback and remove rifle

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -7,6 +7,7 @@ var front_gun_first_pos
 var back_gun_first_pos
 var front_gun_reverse_marker
 var back_gun_reverse_marker
+var gun_first_pos
 
 func _ready():
 	front_gun = $spr_front_gun
@@ -16,6 +17,7 @@ func _ready():
 	back_gun_reverse_marker = $back_gun_reverse_pos
 	front_gun_first_pos = front_gun.position
 	back_gun_first_pos = back_gun.position
+	gun_first_pos = position
 	
 func _process(delta):
 	if get_global_mouse_position().x >= global_position.x:       # GUNS SPRITES FLIP
@@ -29,8 +31,15 @@ func _process(delta):
 		front_gun.flip_v = true
 		back_gun.flip_v = true
 		
-	if Input.is_action_pressed("fire"):
+func _input(event):
+	if event is InputEventMouseButton:
 		var tween = create_tween()
-		
+		if get_global_mouse_position().x >= global_position.x:  
+			tween.tween_property(self, "position", position + Vector2(-5, 0), 0.05).from(position)
+			tween.tween_property(self, "position", position + Vector2(0, 0), 0.05).from(position)
+		else:
+			tween.tween_property(self, "position", position + Vector2(5, 0), 0.05).from(position)
+			tween.tween_property(self, "position", position + Vector2(0, 0), 0.05).from(position)
+			
 		
 	look_at(get_global_mouse_position())           # ROTATE TO THE MOUSE POS
--- scripts/kovboy.gd
+++ scripts/kovboy.gd
@@ -6,50 +6,28 @@ var front_gun
 var back_gun
 var gun_anim
 var small_gun_ready
-var rifle_ready
-@onready var small_bullet = preload("res://scenes/bullet_small.tscn")
-@onready var big_bullet = preload("res://scenes/bullet_rifle.tscn")
+@onready var small_bullet = preload("res://scenes/bullet_rifle.tscn")
 var head
 var head_first_pos
 var head_reverse_pos
 var gun
-var rifle
-
 
 func _ready():
 	head = $spr_head
 	head_first_pos = head.global_position
 	small_gun_ready = true
-	rifle_ready = true
 	head_first_pos = $head_first_pos.position
 	head_reverse_pos = $head_reverse_pos.position
 	gun = $gun
-	rifle = $rifle
 	
 func _process(delta):
 	
-	if Input.is_action_just_pressed("change_weapon"):
-		if gun.visible:
-			gun.visible = false
-			rifle.visible = true
-		elif rifle.visible:
-			gun.visible = true
-			rifle.visible = false # GITHUB TEST AGAIN FROM WORK
-	
-	if gun.visible:                              # ------------ FIRE MECHANISM
-		if Input.is_action_pressed("fire"):
-			if global_variables.small_bullet > 0 and small_gun_ready:
-				var small_bullet_instance = small_bullet.instantiate()
-				small_bullet_instance.global_position = global_position
-				get_parent().add_child(small_bullet_instance)
-				small_gun_ready = false
-	if rifle.visible:
-		if Input.is_action_pressed("fire"):
-			if global_variables.big_bullet > 0 and rifle_ready:
-				var big_bullet_instance = big_bullet.instantiate()
-				big_bullet_instance.global_position = $rifle_bullet_pos.global_position
-				get_parent().add_child(big_bullet_instance)
-				rifle_ready = false
+	if Input.is_action_pressed("fire"):
+		if global_variables.small_bullet > 0 and small_gun_ready:
+			var small_bullet_instance = small_bullet.instantiate()
+			small_bullet_instance.global_position = global_position - Vector2(0, 4)
+			get_parent().add_child(small_bullet_instance)
+			small_gun_ready = false
 			
 	if get_global_mouse_position().x >= global_position.x:   # --------- FLIP SPRITES
 		$spr_body.flip_h = false
@@ -77,10 +55,6 @@ func _physics_process(delta):         # ---------- PHYSICS PROCESS
 	velocity = velocity.normalized() * speed
 	
 	move_and_slide()
-	
 
 func _on_timer_small_gun_timeout():
 	small_gun_ready = true
-
-func _on_timer_rifle_timeout():
-	rifle_ready = true
