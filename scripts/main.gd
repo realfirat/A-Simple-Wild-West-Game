a7c449287a52cb8a49454607f74f86344e286c9d
Fırat Sakar <firatsakar5@gmail.com>
2023-08-17 13:27:13 +0300

    mouse cursors 2
    fix aim center issue

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,7 +5,7 @@ var aim = load("res://assets/_0000_aim.png")
 var hammer = load("res://assets/hammer.png")
 
 func _ready():
-	Input.set_custom_mouse_cursor(aim)
+	Input.set_custom_mouse_cursor(aim, 0, Vector2(25, 25))
 
 func _process(delta):
 	if Input.is_action_just_pressed("escape"):
@@ -15,12 +15,12 @@ func _process(delta):
 		if $UI/building_grid.visible:
 			$UI/building_grid.visible = false
 			if ready_to_change_cursor:
-				Input.set_custom_mouse_cursor(aim)
+				Input.set_custom_mouse_cursor(aim, 0, Vector2(25, 25))
 				ready_to_change_cursor = false
 		else:
 			$UI/building_grid.visible = true
 			if ready_to_change_cursor:
-				Input.set_custom_mouse_cursor(hammer)
+				Input.set_custom_mouse_cursor(hammer, 0, Vector2(25, 10))
 				ready_to_change_cursor = false
 	
 	ready_to_change_cursor = true
