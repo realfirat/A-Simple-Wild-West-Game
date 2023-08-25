d8a1e527d1fead59d9c24621368ce903aac5fda1
Fırat Sakar <firatsakar5@gmail.com>
2023-08-25 14:19:13 +0300

    federal soldier pause

--- scripts/federal.gd
+++ scripts/federal.gd
@@ -5,7 +5,8 @@ var fed_speed = 0.0015
 var hp = 20
 
 func _physics_process(delta):
-	$PathFollow2D.progress_ratio += fed_speed
+	if not global_variables.is_build_mode:
+		$PathFollow2D.progress_ratio += fed_speed
 
 	if $PathFollow2D.progress_ratio > 0.99:
 		queue_free()
@@ -15,13 +16,14 @@ func _physics_process(delta):
 	if hp <= 0:
 		queue_free()
 
-
 func _on_timer_timeout():
-	var ready_bullet = bullet.instantiate()
-	ready_bullet.global_position = $PathFollow2D/federal_body/Sprite2D.global_position
-	add_child(ready_bullet)
+	if not global_variables.is_build_mode:
+		var ready_bullet = bullet.instantiate()
+		ready_bullet.global_position = $PathFollow2D/federal_body/Sprite2D.global_position
+		add_child(ready_bullet)
 
 
 func _on_federal_hitarea_area_entered(area):
 	if area.is_in_group("bullet"):
 		hp -= 5
+		area.queue_free()
