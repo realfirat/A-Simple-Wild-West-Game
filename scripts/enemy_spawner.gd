93dd76ac938e4ff091932500cb24be236071f923
Fırat Sakar <firatsakar5@gmail.com>
2023-08-26 15:26:38 +0300

    fed native bandit reverse

--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -3,7 +3,10 @@ extends Node2D
 @onready var fed = preload("res://scenes/federal.tscn")
 @onready var native = preload("res://scenes/native.tscn")
 @onready var bandit = preload("res://scenes/bandit.tscn")
-@onready var enemies = [fed, native, bandit]
+@onready var fed_reverse = preload("res://scenes/federal_reverse.tscn")
+@onready var native_reverse = preload("res://scenes/native_reverse.tscn")
+@onready var bandit_reverse = preload("res://scenes/bandit_reverse.tscn")
+@onready var enemies = [fed, native, bandit, native_reverse, bandit_reverse, fed_reverse]
 
 
 func _physics_process(delta):
---
+++ scripts/native_reverse.gd
@@ -0,0 +1,28 @@
+extends Path2D
+
+var fed_speed = -0.0025
+@onready var balta = preload("res://scenes/balta.tscn")
+var hp = 20
+
+func _physics_process(delta):
+	if not global_variables.is_build_mode:
+		$PathFollow2D.progress_ratio += fed_speed
+
+	if $PathFollow2D.progress_ratio < 0.01:
+		queue_free()
+		
+	$PathFollow2D/native_body/hp_bar.value = hp
+	
+	if hp <= 0:
+		queue_free()
+	
+func _on_timer_timeout():
+	if not global_variables.is_build_mode:
+		var ready_balta = balta.instantiate()
+		ready_balta.global_position = $PathFollow2D/native_body/Sprite2D.global_position
+		add_child(ready_balta)
+
+func _on_native_hitarea_area_entered(area):
+	if area.is_in_group("bullet_rifle"):
+		hp -= 5
+		area.queue_free()
